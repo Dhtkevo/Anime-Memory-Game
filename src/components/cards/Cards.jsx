ea310c501370091edb8ee8a0d0b0ed91a2cca2bd
Kevin <110263079+Dhtkevo@users.noreply.github.com>
2023-06-29 20:59:29 -0400

    Implement box shadow for cards:

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -10,7 +10,7 @@ Cards.propTypes = {
 export default function Cards({ title, img }) {
     return (
         <>
-            <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 p-1 gap-8 flex flex-col border-2 border-slate-40 rounded-2xl w-1/4 h-1/4 w-full h-full">
+            <div className="hover:shadow-2xl hover:shadow-slate-200 bg-gradient-to-r from-cyan-500 to-blue-500 p-1 gap-8 flex flex-col border-2 border-slate-40 rounded-2xl w-1/4 h-1/4 w-full h-full">
                 <img className="h-2/3 rounded-xl" src={img}></img>
                 <h1 className='text-3xl italic text-center font-bold'>{title}</h1>
 
